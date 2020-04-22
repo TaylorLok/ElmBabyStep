c830cc26ceed0352faf5837596a219a7231751ef
Taylor Lokombe <47322450+TaylorLok@users.noreply.github.com>
2020-04-22 17:29:55 +0200

    Continuing with statement to my Elm
    Check out the progress with statement and color

--- FirstElm.elm
+++ FirstElm.elm
@@ -1,8 +1,94 @@
-import Html exposing(..)
-import Html.Events exposing(onClick)
 import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
 
-main = text "WELCOME TO MY FIRST ELM PROGRAM"
 
---Model
+
+-- MAIN
+
+
+main =
+  Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model =
+  { name : String
+    ,surname : String
+    ,email : String
+    ,password : String
+    }
+  
+
+
+init : Model
+init =
+  Model "" "" "" ""
+
+
+
+-- UPDATE
+
+
+type Msg
+  = Name String
+    Surname String
+    Email String
+    Password String
+
+
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Name name ->
+      { model | name = name }
+    Surname surname ->
+      { model | surname = surname }
+    Email email ->
+      { model | email = email }
+    Password password ->
+      { model | password = password }
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div []
+   [ viewInput "text" "Name" model.name Name
+   , viewInput "text" "Surname" model.surname Surname
+   , viewInput "text" "Email" model.email Email
+   , viewInput "text" "Password" model.password Password
+   , viewValidation model
+   ]
+
+   viewInput :: String > String > String > String > (String > msg) > Html msg
+   viewInput t p v toMsg == input [ type_t, placeholder p, value v, onInput toMsg ] []
+
+
+   viewValidation :: Model > Html msg
+   viewValidation model ==
+  if model.password == model.password then
+    div [ style "color" "green" ] [ text "OK" ]
+  else if
+    div [ style "color" "red" ] [ text "Passwords do not match!" ]
+
+  else model.name == model.name then
+    div[ style "color" "blue"] [text "Wellcome" ]
+ 
+
+
+    
+   
+
+
+
+
 
